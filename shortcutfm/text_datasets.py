838c52cfef7a4b21d18c4ce6056849a44d11aa70
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2025-02-26 20:17:36 +0100

    Feat: Changes to preprocessing data

--- shortcutfm/text_datasets.py
+++ shortcutfm/text_datasets.py
@@ -48,8 +48,6 @@ def load_data_text(
 
     dataset = TextDataset(
         training_data,
-        data_args,
-        model_emb=model_emb
     )
 
     if split != 'test':
@@ -79,17 +77,48 @@ def load_data_text(
         return iter(data_loader)
 
 
-def infinite_loader(data_loader):
-    while True:
-        yield from data_loader
+def get_corpus(data_args, seq_len, split='train', loaded_vocab=None, max_examples=None):
+    print('#' * 30, '\nLoading dataset {} from {}...'.format(data_args.dataset, data_args.data_dir))
+
+    sentence_lst = {'src': [], 'trg': []}
+
+    if split == 'train':
+        print('### Loading form the TRAIN set...')
+        path = f'{data_args.data_dir}/train.jsonl'
+    elif split == 'valid':
+        print('### Loading form the VALID set...')
+        path = f'{data_args.data_dir}/valid.jsonl'
+    elif split == 'test':
+        print('### Loading form the TEST set...')
+        path = f'{data_args.data_dir}/test.jsonl'
+    else:
+        assert False, "invalid split for dataset"
+
+    with open(path, 'r') as f_reader:
+        for i, row in enumerate(f_reader):
+            if max_examples is not None and i >= max_examples:
+                break
+            content = json.loads(row)
+            sentence_lst['src'].append(content['src'].strip())
+            sentence_lst['trg'].append(content['trg'].strip())
+
+    print('### Data samples...\n', sentence_lst['src'][:2], sentence_lst['trg'][:2])
+
+    # get tokenizer.
+    vocab_dict = loaded_vocab
+    if data_args.merge_strategy == "equal":
+        train_dataset = helper_tokenize_fmseq(sentence_lst, vocab_dict, seq_len)
+    else:
+        train_dataset = helper_tokenize(sentence_lst, vocab_dict, seq_len)
+    return train_dataset
 
 
 def helper_tokenize(sentence_lst, vocab_dict, seq_len):
     # Process.memory_info is expressed in bytes, so convert to megabytes
-    print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
+    # print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
     raw_datasets = Dataset2.from_dict(sentence_lst)
     print(raw_datasets)
-    print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
+    # print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
 
     def tokenize_function(examples):
         input_id_x = vocab_dict.encode_token(examples['src'])
@@ -131,8 +160,9 @@ def helper_tokenize(sentence_lst, vocab_dict, seq_len):
             src_len += len(src)
             trg_len += len(trg)
 
-            lst.append(src + [vocab_dict.sep_token_id] + trg)
-            mask.append([0] * (len(src) + 1))
+            # TODO: last token of src is sep anyway, no need to add another one when usnig bert tokenizer
+            lst.append(src + trg)
+            mask.append([0] * (len(src)))
         group_lst['input_ids'] = lst
         group_lst['input_mask'] = mask
         # print(f"src_len: {src_len}, trg_len: {trg_len}, num: {len(mask)}")
@@ -150,11 +180,13 @@ def helper_tokenize(sentence_lst, vocab_dict, seq_len):
 
     def pad_function(group_lst):
         max_length = seq_len
-        group_lst['input_ids'] = _collate_batch_helper(group_lst['input_ids'], vocab_dict.pad_token_id, max_length)
+        seqs, padding_mask = _collate_batch_helper(group_lst['input_ids'], vocab_dict.pad_token_id, max_length, return_mask=True)
+        group_lst['input_ids'] = seqs
+        group_lst['padding_mask'] = padding_mask
         group_lst['input_mask'] = _collate_batch_helper(group_lst['input_mask'], 1, max_length)
         return group_lst
 
-    print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
+    # print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
 
     lm_datasets = tokenized_datasets.map(
         pad_function,
@@ -166,12 +198,24 @@ def helper_tokenize(sentence_lst, vocab_dict, seq_len):
     print(lm_datasets, 'padded dataset')
     print('### padded dataset...example', lm_datasets['input_ids'][0], lm_datasets['input_mask'][0])
 
-    print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
+    # print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
 
     raw_datasets = datasets.DatasetDict()
     raw_datasets['train'] = lm_datasets
-    print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
+    # print(f"RAM used: {psutil.Process().memory_info().rss / (1024 * 1024):.2f} MB")
     return raw_datasets
+
+
+def _collate_batch_helper(examples, pad_token_id, max_length, return_mask=False):
+    result = torch.full([len(examples), max_length], pad_token_id, dtype=torch.int64).tolist()
+    mask_ = torch.full([len(examples), max_length], pad_token_id, dtype=torch.int64).tolist()
+    for i, example in enumerate(examples):
+        curr_len = min(len(example), max_length)
+        result[i][:curr_len] = example[:curr_len]
+        mask_[i][:curr_len] = [1] * curr_len
+    if return_mask:
+        return result, mask_
+    return result
 
 
 def helper_tokenize_fmseq(sentence_lst, vocab_dict, seq_len):
@@ -257,75 +301,23 @@ def helper_tokenize_fmseq(sentence_lst, vocab_dict, seq_len):
     return raw_datasets
 
 
-def get_corpus(data_args, seq_len, split='train', loaded_vocab=None, max_examples=None):
-    print('#' * 30, '\nLoading dataset {} from {}...'.format(data_args.dataset, data_args.data_dir))
-
-    sentence_lst = {'src': [], 'trg': []}
-
-    if split == 'train':
-        print('### Loading form the TRAIN set...')
-        path = f'{data_args.data_dir}/train.jsonl'
-    elif split == 'valid':
-        print('### Loading form the VALID set...')
-        path = f'{data_args.data_dir}/valid.jsonl'
-    elif split == 'test':
-        print('### Loading form the TEST set...')
-        path = f'{data_args.data_dir}/test.jsonl'
-    else:
-        assert False, "invalid split for dataset"
-
-    with open(path, 'r') as f_reader:
-        for i, row in enumerate(f_reader):
-            if max_examples is not None and i > max_examples:
-                break
-            content = json.loads(row)
-            sentence_lst['src'].append(content['src'].strip())
-            sentence_lst['trg'].append(content['trg'].strip())
-
-    print('### Data samples...\n', sentence_lst['src'][:2], sentence_lst['trg'][:2])
-
-    # get tokenizer.
-    vocab_dict = loaded_vocab
-    if data_args.merge_strategy == "equal":
-        train_dataset = helper_tokenize_fmseq(sentence_lst, vocab_dict, seq_len)
-    else:
-        train_dataset = helper_tokenize(sentence_lst, vocab_dict, seq_len)
-    return train_dataset
+def infinite_loader(data_loader):
+    while True:
+        yield from data_loader
 
 
 class TextDataset(Dataset):
-    def __init__(self, text_datasets, data_args, model_emb=None):
+    def __init__(self, text_dataset):
         super().__init__()
-        self.text_datasets = text_datasets
-        self.length = len(self.text_datasets['train'])
-        self.data_args = data_args
-        self.model_emb = model_emb
+        self.text_dataset = text_dataset
+        self.length = len(self.text_dataset)
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
-        with torch.no_grad():
-            input_ids = self.text_datasets['train'][idx]['input_ids']
-            hidden_state = self.model_emb(torch.tensor(input_ids))
-
-            # obtain the input vectors, only used when word embedding is fixed (not trained end-to-end)
-            arr = np.array(hidden_state, dtype=np.float32)
-
-            out_kwargs = {}
-            out_kwargs['input_ids'] = np.array(self.text_datasets['train'][idx]['input_ids'])
-            out_kwargs['input_mask'] = np.array(self.text_datasets['train'][idx]['input_mask'])
-
-            return arr, out_kwargs
-
-
-def _collate_batch_helper(examples, pad_token_id, max_length, return_mask=False):
-    result = torch.full([len(examples), max_length], pad_token_id, dtype=torch.int64).tolist()
-    mask_ = torch.full([len(examples), max_length], pad_token_id, dtype=torch.int64).tolist()
-    for i, example in enumerate(examples):
-        curr_len = min(len(example), max_length)
-        result[i][:curr_len] = example[:curr_len]
-        mask_[i][:curr_len] = [1] * curr_len
-    if return_mask:
-        return result, mask_
-    return result
+        return {
+            "seqs": torch.tensor(self.text_dataset[idx]['input_ids']),
+            "padding_mask": torch.tensor(self.text_dataset[idx]['padding_mask']),
+            "input_ids_mask": torch.tensor(self.text_dataset[idx]['input_mask']),
+        }
