ad4f697b900989098dc8e547d1353b64194448ee
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2025-03-02 23:59:42 +0100

    Feat: Introduce Uniform Sampler

--- shortcutfm/shortcut_samplers.py
+++ shortcutfm/shortcut_samplers.py
@@ -1,3 +1,5 @@
+from abc import ABC, abstractmethod
+
 import torch
 import numpy as np
 
@@ -55,3 +57,52 @@ class TimeAndShorcutStampler:
 
         timesteps = indices * shortcut_values
         return timesteps.to(torch.long), shortcut_values.to(torch.long)
+
+
+class ScheduleSampler(ABC):
+    """
+    A distribution over timesteps in the diffusion process, intended to reduce
+    variance of the objective.
+
+    By default, samplers perform unbiased importance sampling, in which the
+    objective's mean is unchanged.
+    However, subclasses may override sample() to change how the resampled
+    terms are reweighted, allowing for actual changes in the objective.
+    """
+    def __call__(self, batch_size, device):
+        return self.sample(batch_size, device)
+
+    @abstractmethod
+    def weights(self):
+        """
+        Get a numpy array of weights, one per diffusion step.
+
+        The weights needn't be normalized, but must be positive.
+        """
+
+    def sample(self, batch_size, device):
+        """
+        Importance-sample timesteps for a batch.
+
+        :param batch_size: the number of timesteps.
+        :param device: the torch device to save to.
+        :return: a tuple (timesteps, weights):
+                 - timesteps: a tensor of timestep indices.
+                 - weights: a tensor of weights to scale the resulting losses.
+        """
+        w = self.weights()
+        p = w / np.sum(w)
+        indices_np = np.random.choice(len(p), size=(batch_size,), p=p)
+        indices = torch.from_numpy(indices_np).long().to(device)
+        weights_np = 1 / (len(p) * p[indices_np])
+        weights = torch.from_numpy(weights_np).float().to(device)
+        return indices+1, weights
+
+
+class UniformSampler(ScheduleSampler):
+    def __init__(self, diffusion_steps):
+        self.diffusion_steps = diffusion_steps
+        self._weights = np.ones([diffusion_steps])
+
+    def weights(self):
+        return self._weights
