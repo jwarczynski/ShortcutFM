0820e48d3de22a78caee58e91dc89cf2f31abed4
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2025-03-03 00:08:46 +0100

    Fix: typo

--- shortcutfm/train/pl/train_unit.py
+++ shortcutfm/train/pl/train_unit.py
@@ -19,13 +19,13 @@ class TrainModule(pl.LightningModule):
             criterion: Criterion,
             optimizer_config: SchedulerConfig,
             prediction_strategy: Optional[PredictionStrategy] = None,
-            prediction_shorcut_size: int = 1,
+            prediction_shortcut_size: int = 1,
     ) -> None:
         super().__init__()
         self.criterion = criterion
         self.optimizer_config = optimizer_config
         self.prediction_strategy = prediction_strategy
-        self.prediction_shorcut_size = prediction_shorcut_size
+        self.prediction_shortcut_size = prediction_shortcut_size
         self.save_hyperparameters(ignore=['criterion', 'prediction_strategy'])
 
     def forward(self, batch: EncoderBatch) -> dict[str, Tensor]:
@@ -57,7 +57,7 @@ class TrainModule(pl.LightningModule):
                 - input_ids: Input token sequences [batch_size, seq_len]
                 - predictions: Model predictions [batch_size, num_steps, seq_len]
         """
-        predictions = self.criterion.denoise(batch, self.prediction_shorcut_size)
+        predictions = self.criterion.denoise(batch, self.prediction_shortcut_size)
         return batch.seqs, predictions
 
     def _predict_step(self, batch: EncoderBatch, batch_idx: int) -> ndarray[str, dtype[str]]:
@@ -67,7 +67,7 @@ class TrainModule(pl.LightningModule):
         return self.prediction_strategy(batch, self.shortcut_size)
 
     def set_prediction_shorcut_size(self, shortcut_size: int) -> None:
-        self.prediction_shorcut_size = shortcut_size
+        self.prediction_shortcut_size = shortcut_size
 
     def configure_optimizers(self):
         optimizer = AdamW(
