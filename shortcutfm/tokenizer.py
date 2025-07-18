cdf782e34250974fe72dee9cd9b42b12bbdbc7a2
Jędrzej Warczyński <jedrzej.warczynski@student.put.poznan.pl>
2025-07-18 21:36:05 +0200

    fixup! Add tokenization of WMT19 de-en dataset for machine translation

--- shortcutfm/tokenizer.py
+++ shortcutfm/tokenizer.py
@@ -1,8 +1,5 @@
-import json
-import os
-
 import torch
-from transformers import AutoTokenizer, PreTrainedTokenizerFast
+from transformers import AutoTokenizer, PreTrainedTokenizerBase
 
 
 class MyTokenizer:
@@ -24,25 +21,13 @@ class MyTokenizer:
             # save
             if not is_eval:
                 tokenizer.save_pretrained(args.checkpoint_path)
-        else:
-            # load vocab from the path
-            print("#" * 30, "load vocab from", args.vocab)
-            vocab_dict = {"[START]": 0, "[END]": 1, "[UNK]": 2, "[PAD]": 3, "[NULL]": 4}
-            with open(args.vocab, encoding="utf-8") as f:
-                for row in f:
-                    vocab_dict[row.strip().split(" ")[0]] = len(vocab_dict)
-            self.tokenizer = vocab_dict
-            self.rev_tokenizer = {v: k for k, v in vocab_dict.items()}
-            self.sep_token_id = vocab_dict["[END]"]
-            self.pad_token_id = vocab_dict["[PAD]"]
-            self.null_token_id = vocab_dict["[NULL]"]
-            # save
-            if int(os.environ["LOCAL_RANK"]) == 0 and not is_eval:
-                path_save_vocab = f"{args.checkpoint_path}/vocab.json"
-                with open(path_save_vocab, "w") as f:
-                    json.dump(vocab_dict, f)
+        elif args.vocab == "mt":
+            self.tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-de")
+            self.sep_token_id = self.tokenizer.convert_tokens_to_ids("</s>")
+            self.pad_token_id = self.tokenizer.convert_tokens_to_ids("<pad>")
+            self.null_token_id = self.tokenizer.convert_tokens_to_ids("<unk>")
 
-        self.vocab_size = len(self.tokenizer)
+        self.vocab_size = self.tokenizer.vocab_size
         args.vocab_size = self.vocab_size  # update vocab size in args
         args.null_token_id = self.null_token_id  # update null token id in args
 
@@ -51,7 +36,7 @@ class MyTokenizer:
             input_ids = [
                 [0] + [self.tokenizer.get(x, self.tokenizer["[UNK]"]) for x in seq.split()] + [1] for seq in sentences
             ]
-        elif isinstance(self.tokenizer, PreTrainedTokenizerFast):
+        elif isinstance(self.tokenizer, PreTrainedTokenizerBase):
             input_ids = self.tokenizer(sentences, add_special_tokens=True)["input_ids"]
         else:
             raise ValueError("invalid type of vocab_dict")
@@ -63,7 +48,7 @@ class MyTokenizer:
             while len(seq) > 0 and seq[-1] == self.pad_token_id:
                 seq.pop()
             tokens = " ".join([self.rev_tokenizer[x] for x in seq]).replace("__ ", "").replace("@@ ", "")
-        elif isinstance(self.tokenizer, PreTrainedTokenizerFast):
+        elif isinstance(self.tokenizer, PreTrainedTokenizerBase):
             seq = seq.squeeze(-1).tolist()
             while len(seq) > 0 and seq[-1] == self.pad_token_id:
                 seq.pop()
@@ -84,7 +69,7 @@ class MyTokenizer:
                 return self.decode_token(torch.tensor(seq).unsqueeze(-1))
 
             tokens = " ".join([self.rev_tokenizer[x] for x in seq]).replace("__ ", "").replace("@@ ", "")
-        elif isinstance(self.tokenizer, PreTrainedTokenizerFast):
+        elif isinstance(self.tokenizer, PreTrainedTokenizerBase):
             seq = seq.squeeze(-1).cpu().detach().numpy()
             idx = (seq == self.sep_token_id).argmax()
             length = idx + 1
